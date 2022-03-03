9f05a2938e312cbe545916ee37d5a654e64c46b3
Jangsuji <gist.acsl.suji@gmail.com>
2022-03-03 16:01:57 +0900

    add DP

---
+++ Advanced/9. 동적계획법의 소개/이항계수.py
@@ -0,0 +1,32 @@
+'''
+============================================
+[입력]
+3
+2 1 1
+3 2 1
+5 3 2
+
+[출력]
+#1 2
+#2 3
+#3 10
+============================================
+'''
+
+def bino2(n,k):
+    for i in range(n+1):
+        for j in range(min(i,k)+1):
+            if j==0 or j==i:
+                B[i][j] = 1
+            else:
+                B[i][j] = B[i-1][j-1] + B[i-1][j]
+    return B[n][k]
+
+test_case = int(input())
+
+for t in range(test_case):
+
+    n,a,b = map(int, input().split())
+    B = [[0 for _ in range(n+1)] for _ in range(n+1)]
+    print("#{} {}".format(t+1,bino2(n,b)))
+
