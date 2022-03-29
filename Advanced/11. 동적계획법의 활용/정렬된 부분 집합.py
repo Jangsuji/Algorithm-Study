29b8f37949bf39e2203ad9b40d3e70b2ea268a7a
Jangsuji <gist.acsl.suji@gmail.com>
2022-03-29 20:37:24 +0900

    add -A

--- Advanced/11. 동적계획법의 활용/정렬된 부분 집합.py
+++ Advanced/11. 동적계획법의 활용/정렬된 부분 집합.py
@@ -12,3 +12,20 @@
 #3 4
 ============================================
 '''
+
+test_case = int(input())
+for t in range(test_case):
+
+        subset = list(map(int, input().split()))
+        N = subset.pop(0)
+        subset.insert(0,0)
+        opt = list(range(0,N+1))
+        metrix = [[0 for _ in range(N+1)] for _ in range(N+1)]
+        
+        for i in opt[1:]:
+                for j in opt[1:]:
+                        if subset[i] == opt[j]:
+                                metrix[i][j] = metrix[i-1][j-1]+1
+                        else:
+                                metrix[i][j] = max(metrix[i-1][j],metrix[i-1][j-1],metrix[i][j-1])
+        print('#{} {}'.format(t+1,metrix[-1][-1]))
